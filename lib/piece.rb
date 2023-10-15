33faf78c40e39b8a365398fed9531b2458f7f45d
SandyWezzy <72725989+SandyWezzy@users.noreply.github.com>
2023-10-15 13:09:57 +1100

    created pawn class

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,86 +1,114 @@
 class Piece
-    attr_reader :color, :symbol
+  attr_reader :color, :symbol
 
-    def initialize(color)
-        @color = color      # Either white or black
-        @symbol = color == :white ? '♔' : '♚'
-    end
-end 
+  def initialize(color)
+    @color = color      # Either white or black
+    @symbol = color == :white ? '♔' : '♚'
+  end
+end
 
 class King < Piece
-    def initialize(color)
-        @color = color
-        @symbol = color == :white ? '♔' : '♚'
-    end
+  def initialize(color)
+    @color = color
+    @symbol = color == :white ? '♔' : '♚'
+  end
 
-    def valid_moves(current_position, target_position)
-        row_difference = (current_position[0] - target_position[0]).abs
-        col_dfference = (current_position[1] - current_position[1]).abs
+  def valid_moves(current_position, target_position)
+    row_difference = (current_position[0] - target_position[0]).abs
+    col_dfference = (current_position[1] - current_position[1]).abs
 
-        # King can move 1 in any direction
-        return true if row_difference <= 1 && col_dfference <= 1
-        false
+    # King can move 1 in any direction
+    return true if row_difference <= 1 && col_dfference <= 1
 
-        # Note: Casteling defined in Board
-    end
+    false
+
+    # NOTE: Casteling defined in Board
+  end
 end
 
 class Queen < Piece
-    def initialize(color)
-        @color = color
-        @symbol = color == :white ? '♕' : '♛'
-    end
+  def initialize(color)
+    @color = color
+    @symbol = color == :white ? '♕' : '♛'
+  end
 
-    def valid_moves(current_position, target_position)
-        horizontal = current_position[1] == target_position[1]
-        vertical = current_position[0] == target_position[0]
-        diagonal = (current_position[0] - current_position[1]) == (target_position[0] - target_position[1])
+  def valid_moves(current_position, target_position)
+    horizontal = current_position[1] == target_position[1]
+    vertical = current_position[0] == target_position[0]
+    diagonal = (current_position[0] - current_position[1]) == (target_position[0] - target_position[1])
 
-        return true if horizontal || vertical || diagonal
-        false
-    end
+    return true if horizontal || vertical || diagonal
+
+    false
+  end
 end
 
 class Rook < Piece
-    def initialize(color)
-        super(color)
-        @symbol = color == :white ? '♖' : '♜'
-    end
+  def initialize(color)
+    super(color)
+    @symbol = color == :white ? '♖' : '♜'
+  end
 
-    def valid_moves(current_position, target_position)
-        horizontal = current_position[1] == target_position[1]
-        vertical = current_position[0] == target_position[0]
+  def valid_moves(current_position, target_position)
+    horizontal = current_position[1] == target_position[1]
+    vertical = current_position[0] == target_position[0]
 
-        return true if horizontal || vertical
-        false
-    end
+    return true if horizontal || vertical
+
+    false
+  end
 end
 
 class Bishop < Piece
-    def initialize(color)
-        super(color)
-        @symbol = color == :white ? '♗' : '♝'
-    end
+  def initialize(color)
+    super(color)
+    @symbol = color == :white ? '♗' : '♝'
+  end
 
-    def valid_moves(current_position, target_position)
-        diagonal = (current_position[0] - current_position[1]) == (target_position[0] - target_position[1])
+  def valid_moves(current_position, target_position)
+    diagonal = (current_position[0] - current_position[1]) == (target_position[0] - target_position[1])
 
-        return true if diagonal 
-        false
-    end
+    return true if diagonal
+
+    false
+  end
 end
 
 class Knight < Piece
-    def initialize(color)
-        super(color)
-        @symbol = color == :white ? '♘' : '♞'
-    end
+  def initialize(color)
+    super(color)
+    @symbol = color == :white ? '♘' : '♞'
+  end
 
-    def valid_moves(current_position, target_position)
-        row_difference = (current_position[0] - target_position[0]).abs
-        col_difference = (current_position[1] - target_position[1]).abs
+  def valid_moves(current_position, target_position)
+    row_difference = (current_position[0] - target_position[0]).abs
+    col_difference = (current_position[1] - target_position[1]).abs
 
-        return true if (row_difference == 2 && col_difference == 1) || (col_difference == 2 && row_difference == 1)
-        false
-    end
+    return true if (row_difference == 2 && col_difference == 1) || (col_difference == 2 && row_difference == 1)
+
+    false
+  end
+end
+
+class Pawn < Piece
+  def initialize(color)
+    super(color)
+    @symbol = color == :white ? '♙' : '♟'
+  end
+
+  def valid_moves(current_position, target_position)
+    # Two steps move
+    two_steps_move = current_position[1] == 2 || current_position[1] == 7
+
+    # Black is above
+    black_legal_move = color == :black && current_position[1] + 1 == target_position[1]
+    white_legal_move = color == :white && current_position[1] - 1 == target_position[1]
+
+    # One step move
+    one_step_move = black_legal_move || white_legal_move
+
+    return true if one_step_move || two_steps_move
+
+    false
+  end
 end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,118 +1,134 @@
 require_relative '../lib/piece'
 
 describe 'valid moves' do
-    describe 'king' do
+  describe 'king' do
+    # Create instance
+    king = King.new(:black)
 
-        # Create instance
-        king = King.new(:black)
+    it 'allowed moves' do
+      # Horizontal move
+      expect(king.valid_moves([3, 3], [3, 4])).to be true
 
-        it 'allowed moves' do
+      # Vertical move
+      expect(king.valid_moves([3, 3], [3, 2])).to be true
 
-            # Horizontal move
-            expect(king.valid_moves([3,3], [3,4])).to be true
-
-            # Vertical move
-            expect(king.valid_moves([3,3], [3,2])).to be true
-
-            # Diagonal move 
-            expect(king.valid_moves([3,3], [2,2])).to be true
-        end
-
-        it 'ilegal moves' do
-            expect(king.valid_moves([1,1], [4,4])).to be false
-            expect(king.valid_moves([2,2], [5,4])).to be false
-            expect(king.valid_moves([5,5], [2,4])).to be false
-        end
+      # Diagonal move
+      expect(king.valid_moves([3, 3], [2, 2])).to be true
     end
 
-    describe 'queen' do
+    it 'ilegal moves' do
+      expect(king.valid_moves([1, 1], [4, 4])).to be false
+      expect(king.valid_moves([2, 2], [5, 4])).to be false
+      expect(king.valid_moves([5, 5], [2, 4])).to be false
+    end
+  end
 
-        # Create instance
-        queen = Queen.new(:black)
+  describe 'queen' do
+    # Create instance
+    queen = Queen.new(:black)
 
-        it 'allowed moves' do
-            
-            # Horizontal move
-            expect(queen.valid_moves([3,3], [5,3])).to be true
+    it 'allowed moves' do
+      # Horizontal move
+      expect(queen.valid_moves([3, 3], [5, 3])).to be true
 
-            # Vertical move
-            expect(queen.valid_moves([3,3], [3,5])).to be true
+      # Vertical move
+      expect(queen.valid_moves([3, 3], [3, 5])).to be true
 
-            # Diagonal move
-            expect(queen.valid_moves([3,3], [1,1])).to be true
-        end
-
-        it 'ilegal move' do
-            expect(queen.valid_moves([3,3], [1,2])).to be false
-            expect(queen.valid_moves([3,3], [5,4])).to be false
-            expect(queen.valid_moves([3,3], [8,1])).to be false
-        end
+      # Diagonal move
+      expect(queen.valid_moves([3, 3], [1, 1])).to be true
     end
 
-    describe 'rook' do 
+    it 'ilegal move' do
+      expect(queen.valid_moves([3, 3], [1, 2])).to be false
+      expect(queen.valid_moves([3, 3], [5, 4])).to be false
+      expect(queen.valid_moves([3, 3], [8, 1])).to be false
+    end
+  end
 
-        # Create instance
-        rook = Rook.new(:black)
+  describe 'rook' do
+    # Create instance
+    rook = Rook.new(:black)
 
-        it 'allowed moves' do 
+    it 'allowed moves' do
+      # Horizontal move
+      expect(rook.valid_moves([3, 3], [5, 3])).to be true
 
-            # Horizontal move
-            expect(rook.valid_moves([3,3], [5,3])).to be true
-
-            # Vertical move
-            expect(rook.valid_moves([3,3], [3,5])).to be true
-        end
-
-        it 'ilegal moves' do
-            expect(rook.valid_moves([3,3], [1,1])).to be false
-            expect(rook.valid_moves([3,3], [5,1])).to be false
-            expect(rook.valid_moves([3,3], [1,2])).to be false
-        end
+      # Vertical move
+      expect(rook.valid_moves([3, 3], [3, 5])).to be true
     end
 
-    describe 'bishop' do
+    it 'ilegal moves' do
+      expect(rook.valid_moves([3, 3], [1, 1])).to be false
+      expect(rook.valid_moves([3, 3], [5, 1])).to be false
+      expect(rook.valid_moves([3, 3], [1, 2])).to be false
+    end
+  end
 
-        # Create instance
-        bishop = Bishop.new(:black)
+  describe 'bishop' do
+    # Create instance
+    bishop = Bishop.new(:black)
 
-        it 'allowed moves' do
-
-            # Diagonal moves
-            expect(bishop.valid_moves([3,3], [1,1])).to be true
-            expect(bishop.valid_moves([4,2], [3,1])).to be true
-            expect(bishop.valid_moves([5,4], [3,2])).to be true
-        end
-
-        it 'ilegal moves' do
-            expect(bishop.valid_moves([3,3], [1,2])).to be false
-            expect(bishop.valid_moves([4,2], [1,3])).to be false
-            expect(bishop.valid_moves([5,4], [3,3])).to be false
-        end
+    it 'allowed moves' do
+      # Diagonal moves
+      expect(bishop.valid_moves([3, 3], [1, 1])).to be true
+      expect(bishop.valid_moves([4, 2], [3, 1])).to be true
+      expect(bishop.valid_moves([5, 4], [3, 2])).to be true
     end
 
-    describe 'knight' do
-
-        # Create instance
-        knight = Knight.new(:black)
-
-        it 'allowed moves' do 
-            expect(knight.valid_moves([3,3], [4,1])).to be true
-            expect(knight.valid_moves([3,3], [2,1])).to be true
-            expect(knight.valid_moves([3,3], [1,2])).to be true
-            expect(knight.valid_moves([3,3], [5,2])).to be true
-            expect(knight.valid_moves([3,3], [5,4])).to be true
-            expect(knight.valid_moves([3,3], [1,4])).to be true
-            expect(knight.valid_moves([3,3], [2,5])).to be true
-            expect(knight.valid_moves([3,3], [4,5])).to be true
-        end
-
-        it 'ilegal moves' do
-            expect(knight.valid_moves([3,3], [1,1])).to be false
-            expect(knight.valid_moves([3,3], [4,3])).to be false
-            expect(knight.valid_moves([3,3], [2,2])).to be false
-            expect(knight.valid_moves([3,3], [2,3])).to be false
-            expect(knight.valid_moves([3,3], [1,5])).to be false
-        end
+    it 'ilegal moves' do
+      expect(bishop.valid_moves([3, 3], [1, 2])).to be false
+      expect(bishop.valid_moves([4, 2], [1, 3])).to be false
+      expect(bishop.valid_moves([5, 4], [3, 3])).to be false
     end
-            
+  end
+
+  describe 'knight' do
+    # Create instance
+    knight = Knight.new(:black)
+
+    it 'allowed moves' do
+      expect(knight.valid_moves([3, 3], [4, 1])).to be true
+      expect(knight.valid_moves([3, 3], [2, 1])).to be true
+      expect(knight.valid_moves([3, 3], [1, 2])).to be true
+      expect(knight.valid_moves([3, 3], [5, 2])).to be true
+      expect(knight.valid_moves([3, 3], [5, 4])).to be true
+      expect(knight.valid_moves([3, 3], [1, 4])).to be true
+      expect(knight.valid_moves([3, 3], [2, 5])).to be true
+      expect(knight.valid_moves([3, 3], [4, 5])).to be true
+    end
+
+    it 'ilegal moves' do
+      expect(knight.valid_moves([3, 3], [1, 1])).to be false
+      expect(knight.valid_moves([3, 3], [4, 3])).to be false
+      expect(knight.valid_moves([3, 3], [2, 2])).to be false
+      expect(knight.valid_moves([3, 3], [2, 3])).to be false
+      expect(knight.valid_moves([3, 3], [1, 5])).to be false
+    end
+  end
+
+  describe 'pawn' do
+    # Create instance
+    pawn_black = Pawn.new(:black)
+    pawn_white = Pawn.new(:white)
+
+    it 'allowed moves' do
+      # One step ahead
+      expect(pawn_black.valid_moves([3, 3], [3, 4])).to be true
+      expect(pawn_black.valid_moves([1, 4], [1, 5])).to be true
+      expect(pawn_white.valid_moves([3, 3], [3, 2])).to be true
+
+      # Two steps if first move
+      expect(pawn_black.valid_moves([2, 2], [2, 4])).to be true
+      expect(pawn_white.valid_moves([2, 7], [2, 5])).to be true
+    end
+
+    it 'allowed moves' do
+      # Ilegal horizintal move
+      expect(pawn_black.valid_moves([3, 3], [2, 3])).to be false
+
+      # Ilegal two steps move
+      expect(pawn_black.valid_moves([4, 4], [4, 6])).to be false
+    end
+  end
 end
+
